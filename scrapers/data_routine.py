87c56d91d87a67960cd30bdfcd669a4c34ec1277
Arthur Nery <99993145+r2nery@users.noreply.github.com>
2022-08-23 22:06:39 -0300

    update

--- scrapers/data_routine.py
+++ scrapers/data_routine.py
@@ -1,14 +1,20 @@
-import pandas as pd
 import os
-import regex as re
+import warnings
 import requests
+import numpy as np
+import regex as re
+import pandas as pd
+from lda import LDA
 from datetime import datetime
-from alive_progress import alive_bar
 from bs4 import BeautifulSoup
+from alive_progress import alive_bar
+from gensim.parsing.preprocessing import remove_stopwords
+from sklearn.feature_extraction.text import CountVectorizer
+
+warnings.simplefilter(action="ignore", category=FutureWarning)
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath("__file__"))
-PARENT_DIR = os.path.dirname(ROOT_DIR)
 GUARDIAN_DIR = os.path.join(ROOT_DIR, "data", "Guardian.csv")
 REUTERS_DIR = os.path.join(ROOT_DIR, "data", "Reuters.csv")
 
@@ -42,17 +48,6 @@ class Guardian:
             text = text.replace(i, j)
         return text
 
-    def save(self, dataF):
-        if os.path.exists(GUARDIAN_DIR):
-            existingData = pd.read_csv(GUARDIAN_DIR)
-            data = pd.concat([existingData, dataF])
-            data = data.drop_duplicates(keep="first")
-            data.to_csv(GUARDIAN_DIR, index=False)
-            return data
-        else:
-            dataF.to_csv(GUARDIAN_DIR, index=False)
-            return dataF
-
     def concatData(self, old, new):
         result = pd.concat([old, new])
         result = result.drop_duplicates(subset=["Text"])
@@ -65,7 +60,7 @@ class Guardian:
 
     def scraper(self):
 
-        os.makedirs(os.path.join(PARENT_DIR, "data"), exist_ok=True)
+        os.makedirs(os.path.join(ROOT_DIR, "data"), exist_ok=True)
 
         rep = {
             "Sign up to First Edition, our free daily newsletter – every weekday morning at 7am": "",
@@ -132,7 +127,7 @@ class Guardian:
         else:
             print(f"→ {lenAfter} new articles saved to Guardian.csv! Total articles: {len(data)}")
 
-        data.to_csv(GUARDIAN_DIR, index=True)
+        data.to_csv(GUARDIAN_DIR)
         return data
 
 
@@ -193,7 +188,7 @@ class Reuters:
 
     def URLFetcher(self):
         self.urls = []
-        with alive_bar(title="→ Fetching URLs in pages (estimate)", bar=None, spinner="dots", force_tty=True) as bar:
+        with alive_bar(title="→ Fetching URLs in pages", bar=None, spinner="dots", force_tty=True) as bar:
             for page in range(1, (self.latestPage) + 1):
                 ukr = "https://www.reuters.com/news/archive/ukraine?view=page&page=" + str(page) + "&pageSize=10"
                 rus = "https://www.reuters.com/news/archive/russia?view=page&page=" + str(page) + "&pageSize=10"
@@ -250,7 +245,7 @@ class Reuters:
                     urls.append(url)
                     bar()
                 except Exception as e:
-                    print(f"URL couldn't be parsed: {url} because {e}")
+                    #print(f"URL couldn't be parsed: {url} because {e}")
                     pass
         data = pd.DataFrame({"URL": urls, "Date": dates, "Title": titles, "Text": bodies})
         # print("-> New data fetched successfully!")
@@ -265,18 +260,133 @@ class Reuters:
         print("parsing articles")
         self.articleFetcher()
         data = self.concatData(self.new_data)
-
         lenAfter = len(data) - len(self.reuters_data)
-
         if lenAfter == 0:
             print(f"→ No new articles found. Total articles: {len(data)}")
         else:
             print(f"→ {lenAfter} new articles saved to Reuters.csv! Total articles: {len(data)}")
+        data.to_csv(REUTERS_DIR, index=False)
 
-        data.to_csv(REUTERS_DIR, index=True)
         return data
+
+
+class NTR:
+    def __init__(self) -> None:
+        self.dataG = pd.read_csv(GUARDIAN_DIR)
+        self.dataR = pd.read_csv(REUTERS_DIR)
+        pass
+
+    def learn_topics(self, dataframe, topicnum, vocabsize, num_iter):
+
+        # Removes stopwords
+        texts = dataframe["Text"].tolist()
+        texts_no_sw = []
+        for text in texts:
+            text_no_sw = remove_stopwords(text)
+            texts_no_sw.append(text_no_sw)
+        texts = texts_no_sw
+        CVzer = CountVectorizer(token_pattern=r"(?u)\b[^\W\d]{2,}\b", max_features=vocabsize, lowercase=True)
+        doc_vcnts = CVzer.fit_transform(texts)
+        vocabulary = CVzer.get_feature_names_out()
+        lda_model = LDA(topicnum, n_iter=num_iter, refresh=100)
+        doc_topic = lda_model.fit_transform(doc_vcnts)
+        topic_word = lda_model.topic_word_
+
+        return doc_topic, topic_word, vocabulary
+
+    def save_topicmodel(self, doc_topic, topic_word, vocabulary, source):
+
+        topicmixture_outpath = os.path.join(ROOT_DIR,"results", source + "TopicMixtures.txt")
+        np.savetxt(topicmixture_outpath, doc_topic)
+        topic_outpath = os.path.join(ROOT_DIR,"results",source + "Topics.txt")
+        np.savetxt(topic_outpath, topic_word)
+        vocab_outpath = os.path.join(ROOT_DIR,"results",source + "Vocab.txt")
+        with open(vocab_outpath, mode="w", encoding="utf-8") as f:
+            for v in vocabulary:
+                f.write(v + "\n")
+
+        return topicmixture_outpath, topic_outpath, vocab_outpath
+
+    def KLdivergence_from_probdist_arrays(self, pdists0, pdists1):
+
+        assert pdists0.shape == pdists1.shape, "pdist* shapes must be identical"
+        if len(pdists0.shape) == 1:
+            KLdivs = (pdists1 * np.log2(pdists1 / pdists0)).sum()
+        elif len(pdists0.shape) == 2:
+            KLdivs = (pdists1 * np.log2(pdists1 / pdists0)).sum(axis=1)
+
+        return KLdivs
+
+    def novelty_transience_resonance(self, thetas_arr, scale):
+
+        speechstart = scale
+        speechend = thetas_arr.shape[0] - scale
+        novelties = []
+        transiences = []
+        resonances = []
+        for j in range(speechstart, speechend, 1):
+            center_theta = thetas_arr[j]
+            after_boxend = j + scale + 1
+            before_boxstart = j - scale
+            before_theta_arr = thetas_arr[before_boxstart:j]
+            beforenum = before_theta_arr.shape[0]
+            before_centertheta_arr = np.tile(center_theta, reps=(beforenum, 1))
+            after_theta_arr = thetas_arr[j + 1 : after_boxend]
+            afternum = after_theta_arr.shape[0]
+            after_centertheta_arr = np.tile(center_theta, reps=(afternum, 1))
+            before_KLDs = self.KLdivergence_from_probdist_arrays(before_theta_arr, before_centertheta_arr)
+            after_KLDs = self.KLdivergence_from_probdist_arrays(after_theta_arr, after_centertheta_arr)
+            novelty = np.mean(before_KLDs)
+            transience = np.mean(after_KLDs)
+            novelties.append(novelty)
+            transiences.append(transience)
+            resonances.append(novelty - transience)
+        for index in range(0, scale):
+            transiences.insert(0, 0)
+            transiences.append(0)
+            novelties.insert(0, 0)
+            novelties.append(0)
+            resonances.insert(0, 0)
+            resonances.append(0)
+
+        return novelties, transiences, resonances
+
+    def save_novel_trans_reson(self, novelties, transiences, resonances, source):
+
+        outpath = ROOT_DIR + "/results/" + source + "NovelTransReson.txt"
+        np.savetxt(outpath, np.vstack(zip(novelties, transiences, resonances)))
+
+    def routine(self, period, topicnum, vocabsize, num_iter):
+
+        sources = ["Guardian", "Reuters"]
+        sets = [self.dataG, self.dataR]
+        print("")
+        for i in range(0, len(sources)):
+            data, source = sets[i], sources[i]
+            print(f"→ Starting {source} topic modeling (LDA)...")
+            doc_topic, topic_word, vocabulary = self.learn_topics(data, topicnum, vocabsize, num_iter)
+            topics = []
+            for i in range(len(data)):
+                topics.append(doc_topic[i].argmax())
+            self.save_topicmodel(doc_topic, topic_word, vocabulary, source)
+            novelties, transiences, resonances = self.novelty_transience_resonance(doc_topic, period)
+            self.save_novel_trans_reson(novelties, transiences, resonances, source)
+            ntr_data = data
+            ntr_data["Novelty"] = novelties
+            ntr_data["Transience"] = novelties
+            ntr_data["Resonance"] = resonances
+            ntr_data["Topic"] = topics
+            ntr_data.to_csv(ROOT_DIR + "/data/" + source + "_ntr.csv", index=False)
+
+        print("→ All LDA data saved. Ready for plotting")
 
 
 if __name__ == "__main__":
     Guardian().scraper()
     Reuters().scraper()
+    NTR().routine(
+    period=7,
+    topicnum=30, 
+    vocabsize=10000, 
+    num_iter=100
+    )
