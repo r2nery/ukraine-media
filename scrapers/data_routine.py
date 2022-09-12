2209d4d660931416bfc27cc84dae4d2483f0e8e0
Arthur Nery <99993145+r2nery@users.noreply.github.com>
2022-09-12 01:27:34 -0300

    HuffPost

--- scrapers/data_routine.py
+++ scrapers/data_routine.py
@@ -1,3 +1,4 @@
+from ast import Dict
 import os
 import warnings
 import requests
@@ -29,6 +30,7 @@ from alive_progress import alive_bar
 warnings.simplefilter(action="ignore", category=FutureWarning)
 warnings.simplefilter(action="ignore", category=RuntimeWarning)
 
+
 ROOT_DIR = os.path.dirname(os.path.abspath("__file__"))
 GLOVE_DIR = os.path.join(ROOT_DIR, "glove_data", "results", "vectors.txt")
 GUARDIAN_DIR = os.path.join(ROOT_DIR, "data", "Guardian.csv")
@@ -40,6 +42,8 @@ FOX_DIR = os.path.join(ROOT_DIR, "data", "Fox.csv")
 NBC_DIR = os.path.join(ROOT_DIR, "data", "NBC.csv")
 MIRROR_DIR = os.path.join(ROOT_DIR, "data", "Mirror.csv")
 RT_DIR = os.path.join(ROOT_DIR, "data", "RT.csv")
+EXPRESS_DIR = os.path.join(ROOT_DIR, "data", "Express.csv")
+HUFFPOST_DIR = os.path.join(ROOT_DIR, "data", "HuffPost.csv")
 
 
 class Guardian:
@@ -1136,6 +1140,229 @@ class RT:
         return data
 
 
+class Express:
+    def __init__(self) -> None:
+        self.source = "Express"
+        self.dir = EXPRESS_DIR
+
+    def fromScratch(self):
+        if not os.path.exists(self.dir):
+            self.old_data = pd.DataFrame(columns=["Date", "URL", "Title", "Text"])
+            print(f"-> {self.source}: No CSV file found. Creating...")
+            self.from_scratch = True
+        else:
+            self.old_data = pd.read_csv(self.dir)
+            self.from_scratch = False
+
+    def concatData(self):
+        result = pd.concat([self.old_data, self.new_data])
+        result = result.drop_duplicates(subset=["Text"])
+        result = result.set_index("Date")
+        result = result.sort_index(ascending=False)
+        return result
+
+    def URLFetcher(self):
+        self.urls, self.dates = [], []
+
+        if self.from_scratch == False:
+            last_urls = self.old_data.iloc[0:5, 1].tolist()
+        elif self.from_scratch == True:
+            last_urls = ["https://www.express.co.uk/news/world/1573537/ukraine-russia-cluster-bombing-kills-dozens-civillians"]
+
+        with alive_bar(title=f"-> {self.source}: Fetching URLs in pages", bar=None, spinner="dots", force_tty=True) as bar:
+            sources = ["https://www.express.co.uk/latest/ukraine?pageNumber=", "https://www.express.co.uk/latest/russia?pageNumber="]
+            for source in sources:
+                for page in range(0, 5):  # 600
+                    url = source + str(page)
+                    leading_url = "https://www.express.co.uk"
+                    title_tag = "post"
+                    inc_list = ["/science/", "/world/", "/politics/", "/uk/"]
+                    try:
+                        html_text = requests.get(url).text
+                        soup = BeautifulSoup(html_text, "lxml")
+                        headlines = soup.find_all("li", class_=title_tag)
+                        for headline in headlines:
+                            div = headline.find("div")
+                            _ = div.find("a", href=True)
+                            url = leading_url + _["href"]
+                            if any(s in url for s in inc_list):
+                                self.urls.append(url)
+                                bar()
+                            if url in last_urls:
+                                break
+                        if url in last_urls:
+                            break
+                    except Exception as e:
+                        print(f"Error in page {page}: {e}")
+                        pass
+        self.unique_urls = list(dict.fromkeys(self.urls))
+
+    def articleScraper(self):
+        bodies, titles, dates, urls = [], [], [], []
+        replacements_dict = {"FOLLOW BELOW FOR LIVE UPDATES…": ""}
+
+        def replaceAll(text, dict):
+            for i, j in dict.items():
+                text = text.replace(i, j)
+                text = re.sub(r"[\\].....", "", text)
+            return text
+
+        with alive_bar(len(self.unique_urls), title=f"-> {self.source}: Article scraper", spinner="dots_waves", bar="smooth", force_tty=True) as bar:
+            for url in self.unique_urls:
+                try:
+                    title_box_tags = ["clearfix"]
+                    text_tags = ["text-description"]
+                    exc_p = ["<strong>"]
+                    html_text = requests.get(url).text
+                    soup = BeautifulSoup(html_text, "lxml")
+                    title_box = soup.find("header", class_=title_box_tags)
+                    title = title_box.find("h1").text
+                    date_box = soup.find("time")
+                    date = date_box["datetime"][:10]
+                    blocks = soup.find_all("div", class_=text_tags)
+                    body = ""
+                    for block in blocks:
+                        paragraphs = block.find_all("p")
+                        for p in paragraphs:
+                            if not any(s in str(p) for s in exc_p):
+                                body += " " + p.text
+                    body = replaceAll(body, replacements_dict)
+                    body = " ".join(body.split())
+                    bodies.append(body)
+                    titles.append(title)
+                    urls.append(url)
+                    dates.append(date)
+                    bar()
+                except Exception as e:
+                    print(f"URL couldn't be scraped: {url} because {e}")
+                    pass
+        data = pd.DataFrame({"URL": urls, "Date": dates, "Title": titles, "Text": bodies})
+        self.new_data = data
+
+    def scraper(self):
+        self.fromScratch()
+        self.URLFetcher()
+        self.articleScraper()
+        data = self.concatData()
+        lenAfter = len(data) - len(self.old_data)
+        if lenAfter == 0:
+            print(f"-> No new articles found. Total articles: {len(data)}")
+        else:
+            print(f"-> {lenAfter} new articles saved to {self.source}.csv! Total articles: {len(data)}")
+            print("")
+        data.to_csv(self.dir, index=True)
+
+        return data
+
+
+class Huffpost:
+    def __init__(self) -> None:
+        self.source = "Huffpost"
+        self.dir = HUFFPOST_DIR
+
+    def fromScratch(self):
+        if not os.path.exists(self.dir):
+            self.old_data = pd.DataFrame(columns=["Date", "URL", "Title", "Text"])
+            print(f"-> {self.source}: No CSV file found. Creating...")
+            self.from_scratch = True
+        else:
+            self.old_data = pd.read_csv(self.dir)
+            self.from_scratch = False
+
+    def concatData(self):
+        result = pd.concat([self.old_data, self.new_data])
+        result = result.drop_duplicates(subset=["Text"])
+        result = result.set_index("Date")
+        result = result.sort_index(ascending=False)
+        return result
+
+    def URLFetcher(self):
+        self.urls, self.dates = [], []
+
+        if self.from_scratch == False:
+            last_urls = self.old_data.iloc[0:5, 1].tolist()
+        elif self.from_scratch == True:
+            last_urls = []
+
+        with alive_bar(title=f"-> {self.source}: Fetching URLs in pages", bar=None, spinner="dots", force_tty=True) as bar:
+            sources = ["https://www.huffpost.com/news/topic/ukraine?page=", "https://www.huffpost.com/news/topic/russia?page="]
+            for source in sources:
+                for page in range(0, 10):  # 92
+                    url = source + str(page)
+                    title_tag = "card__headline card__headline--long"
+                    inc_list = ["huffpost"]
+                    try:
+                        html_text = requests.get(url).text
+                        soup = BeautifulSoup(html_text, "lxml")
+                        headlines = soup.find_all("a", class_=title_tag)
+                        for headline in headlines:
+                            url = headline["href"]
+                            if any(s in url for s in inc_list):
+                                self.urls.append(url)
+                                bar()
+                            if url in last_urls:
+                                break
+                        if url in last_urls:
+                            break
+                    except Exception as e:
+                        print(f"Error in page {page}: {e}")
+                        pass
+        self.unique_urls = list(dict.fromkeys(self.urls))
+
+    def articleScraper(self):
+        bodies, titles, dates, urls = [], [], [], []
+        replacements_dict = {}
+
+        def replaceAll(text, dict):
+            for i, j in dict.items():
+                text = text.replace(i, j)
+                text = re.sub(r"[\\].....", "", text)
+            return text
+
+        with alive_bar(len(self.unique_urls), title=f"-> {self.source}: Article scraper", spinner="dots_waves", bar="smooth", force_tty=True) as bar:
+            for url in self.unique_urls:
+                try:
+                    title_box_tags = ["headline"]
+                    text_box_tags = ["entry__content-list js-entry-content js-cet-subunit"]
+                    html_text = requests.get(url).text
+                    soup = BeautifulSoup(html_text, "lxml")
+                    title = soup.find("h1", class_=title_box_tags).text
+                    date_box = soup.find("time")
+                    date = date_box["datetime"][:10]
+                    text_block = soup.find("section", class_=text_box_tags)
+                    paragraphs = text_block.find_all("p")
+                    body = ""
+                    for p in paragraphs:
+                        body += " " + p.text
+                    body = replaceAll(body, replacements_dict)
+                    body = " ".join(body.split())
+                    bodies.append(body)
+                    titles.append(title)
+                    urls.append(url)
+                    dates.append(date)
+                    bar()
+                except Exception as e:
+                    print(f"URL couldn't be scraped: {url} because {e}")
+                    pass
+        data = pd.DataFrame({"URL": urls, "Date": dates, "Title": titles, "Text": bodies})
+        self.new_data = data
+
+    def scraper(self):
+        self.fromScratch()
+        self.URLFetcher()
+        self.articleScraper()
+        data = self.concatData()
+        lenAfter = len(data) - len(self.old_data)
+        if lenAfter == 0:
+            print(f"-> No new articles found. Total articles: {len(data)}")
+        else:
+            print(f"-> {lenAfter} new articles saved to {self.source}.csv! Total articles: {len(data)}")
+            print("")
+        data.to_csv(self.dir, index=True)
+
+        return data
+
+
 class NTR:
     def __init__(self) -> None:
         self.data_guardian = pd.read_csv(GUARDIAN_DIR)
@@ -1347,7 +1574,10 @@ if __name__ == "__main__":
     # DailyMail().scraper() # OK
     # AssociatedPress().scraper() # OK, NEEDS RUN FROM SCRATCH
     # Fox().scraper() # OK
-    # NBC().scraper() # OK, few articles
-    # Mirror().scraper() OK
-    RT().scraper()
+    # NBC().scraper() # OK, few articles (jul-22)
+    # Mirror().scraper() # OK, few articles, needs title fix (feb-22)
+    # RT().scraper() # OK, few articles (jun-22)
+    # Express().scraper()  # OK, NEEDS RUN FROM SCRATCH
+    # Huffpost().scraper() # OK, NEEDS RUN FROM SCRATCH, almost all news are AP
     # NTR().routine(period=7, topicnum=30, vocabsize=10000, num_iter=2000)
+    pass
