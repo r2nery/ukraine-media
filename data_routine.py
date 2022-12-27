008c6661198c8553e0fdce2f8fd322b91667f0a6
Arthur Nery <99993145+r2nery@users.noreply.github.com>
2022-12-27 19:44:14 -0300

    Latest run

--- data_routine.py
+++ data_routine.py
@@ -21,22 +21,22 @@ if __name__ == "__main__":
 
     # Collect dates at url stage
     # add initial date to init of classes
-    CNN().scraper()
-    Guardian().scraper()  # opinion on /commentisfree/
-    Fox().scraper()  # opinion on /opinion/
-    Reuters().scraper()
-    Huffpost().scraper()
-    AP().scraper()
-    # CBS().scraper() # Run from scratch to update
-    ABC().scraper()
-    # NYT().scraper()  # opinion on /opinion/
+    # CNN().scraper()
+    # Guardian().scraper()  # opinion on /commentisfree/
+    # Fox().scraper()  # opinion on /opinion/
+    # Reuters().scraper()
+    # Huffpost().scraper()
+    # AP().scraper()
+    # # CBS().scraper() # Run from scratch to update
+    # ABC().scraper()
+    # # NYT().scraper()  # opinion on /opinion/
 
-    Express().scraper()
-    Mirror().scraper()  # few articles
-    DailyMail().scraper() # comments ok
+    # Express().scraper()
+    # Mirror().scraper()  # few articles
+    # DailyMail().scraper() # comments ok
 
-    utils.unite_sources()
-    # NTR().routine(date_start="2022-03-01", date_end="2022-08-01",
-    #               kld_days_window=2, topicnum=30, vocabsize=10000,
-    #               num_iter=300)
+    # utils.unite_sources()
+    NTR().routine(date_start="2022-03-01", date_end="2022-08-01",
+                  kld_days_window=7, topicnum=30, vocabsize=10000,
+                  num_iter=300)
     
